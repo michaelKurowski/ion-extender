b6cf4cc6a10a818de9323bde8de11e222abde4ca
Michael Kurowski <Byamarro@gmail.com>
2016-11-04 16:29:28 +0100

    Declarative config has been added
    Declarative config
     	new file:   src/config/communicationProtocol.json
    
    Reading a file
     	modified:   src/index.js
    
    Version changes
     	modified:   src/package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 //Util functions for ion apps
 
 console.log("runned")
+
+var fs = require('fs')
+var configFile = fs.readFileSync('/config/communicationProtocol.json', 'utf8')
+
 module.exports = {
 	resSend: function (res, code, additionalData) {
 		function resObjFac(code, description, additionalData){
@@ -15,7 +19,7 @@ module.exports = {
 				res.statusMessage = 'CREATED. The request has been fulfilled, resulting in the creation of a new resource.'
 				break
 			case 401:
-				res.statusMessage = 'UNAUTHORIZED. The request has not been applied because it lacks valid authentication credentials for the target resource..'
+				res.statusMessage = 'UNAUTHORIZED. The request has not been applied because it lacks valid authentication credentials for the target resource.'
 				break
 			case 402:
 				res.statusMessage = 'PAYMENT REQUIRED. The request has not been applied because it has not been authorized by payment.'
@@ -29,3 +33,4 @@ module.exports = {
 		}
 		res.send(additionalData)
 	}
+}
