4e2b47da044292920d7fbb68199b74d586c18255
Michael Kurowski <Byamarro@gmail.com>
2016-11-04 02:49:49 +0100

    Added first code

---
+++ src/index.js
@@ -0,0 +1,31 @@
+//Util functions for ion apps
+
+console.log("runned")
+module.exports = {
+	resSend: function (res, code, additionalData) {
+		function resObjFac(code, description, additionalData){
+			if (additionalData) return {code: code, description: description, additionalData: additionalData}
+			return {code: code, description: description}
+		}
+
+		var description
+		res.statusCode = code
+		switch (code) {
+			case 201:
+				res.statusMessage = 'CREATED. The request has been fulfilled, resulting in the creation of a new resource.'
+				break
+			case 401:
+				res.statusMessage = 'UNAUTHORIZED. The request has not been applied because it lacks valid authentication credentials for the target resource..'
+				break
+			case 402:
+				res.statusMessage = 'PAYMENT REQUIRED. The request has not been applied because it has not been authorized by payment.'
+				break
+			case 404:
+				res.statusMessage = 'NOT FOUND. The origin server did not find a current representation for the target resource or is not willing to disclose that one exists.'
+				break
+			case 409:
+				res.statusMessage = 'CONFLICT. The request could not be completed due to a conflict with the current state of the target resource.'
+				break
+		}
+		res.send(additionalData)
+	}
