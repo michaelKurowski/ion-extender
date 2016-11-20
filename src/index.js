c36788c19f070389f3732861dafbe9f350563ca3
Michael Kurowski <Byamarro@gmail.com>
2016-11-20 22:48:19 +0100

    Added parameter to resSend that determines if you want to send description.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ var conf = require('./config/communicationProtocol.json')
 //var configFile = fs.readFileSync('config/communicationProtocol.json', 'utf8')
 //console.log(  JSON.stringify(conf)  )
 module.exports = {
-	resSend: function (res, nomenclature, additionalData) {
+	resSend: function (res, nomenclature, additionalData, sendDescription) {
+		sendDescription = sendDescription || false
 		function resObjFac(code, description, additionalData) {
 			if (additionalData) return {code: code, description: description, additionalData: additionalData}
 			return {code: code, description: description}
 		}
 		res.statusCode = conf[nomenclature].code
-		res.statusMessage = conf[nomenclature].description
+		if (sendDescription) res.statusMessage = conf[nomenclature].description
 		res.send(JSON.stringify(additionalData))
 	}
 }
